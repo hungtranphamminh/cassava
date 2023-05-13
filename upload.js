b95ef496110e3f193ed30e882776393298a796cc
artemis <tranphamminhhung65@gmail.com>
2023-05-13 18:15:54 +0700

    add col chart

--- upload.js
+++ upload.js
@@ -542,12 +542,91 @@ function resetSecDisplay(){
   })
 }
 
+function showColChart(){
+  var options = {
+    series: [{
+    name: 'PRODUCT 1',
+    data: [44, 55, 41, 67, 22, 43]
+  }, {
+    name: 'PRODUCT 2',
+    data: [13, 23, 20, 8, 13, 27]
+  }, {
+    name: 'PRODUCT 3',
+    data: [11, 17, 15, 15, 21, 14]
+  }, {
+    name: 'PRODUCT 4',
+    data: [21, 7, 25, 13, 22, 8]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350,
+    stacked: true,
+    toolbar: {
+      show: true
+    },
+    zoom: {
+      enabled: true
+    }
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      legend: {
+        position: 'bottom',
+        offsetX: -10,
+        offsetY: 0
+      }
+    }
+  }],
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      borderRadius: 10,
+      dataLabels: {
+        total: {
+          enabled: true,
+          style: {
+            fontSize: '13px',
+            fontWeight: 900
+          }
+        }
+      }
+    },
+  },
+  xaxis: {
+    type: 'datetime',
+    categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
+      '01/05/2011 GMT', '01/06/2011 GMT'
+    ],
+  },
+  legend: {
+    position: 'right',
+    offsetY: 40
+  },
+  fill: {
+    opacity: 1
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#colchart"), options);
+  chart.render();
+
+}
+
+
+
+
+
+// reload with interval
 function pageReload(){
   healthycount=infected=a=b=c=d=sectioncount=0
   resetSecDisplay()
   classifiData(data)
   showPie()
+  
   showStatus(data)
+  showColChart()
+  
 }
 
 pageReload()
@@ -661,26 +740,7 @@ window.onload = () => addZoom("zoomC");
 
 // setInterval(pageReload,300000)
 
-// function testDirect(){
-//   var formdata = new FormData();    
-//   formdata.append("image", fileInput.files[0], imgsrc);
 
-//   var requestOptions = {
-//     method: 'POST',
-//     headers: myHeaders,
-//     body: formdata,
-//     redirect: 'follow'
-//   };
-
-//   //replace YOUR_API_ToCall = url
-
-//   fetch("YOUR_API_ToCall", requestOptions)
-//     .then(response => response.text())
-//     .then(result => console.log(result))
-//     .catch(error => console.log('error', error));
-
-
-// }
 
 
 // const upload = async () => {
